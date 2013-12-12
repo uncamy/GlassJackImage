3bb0e0a6412282609605822f11649a27744da951
Amy Roberts <uncamy@gmail.com>
2013-12-12 17:44:58 +0000

    save image data from post request as img

--- image_handler.py
+++ image_handler.py
@@ -1,18 +1,33 @@
-import SimpleHTTPServer
-import SocketServer
+import requests
+from PIL import Image
+import ipdb
 from flask import Flask, request, session, g, redirect, url_for,\
-                  abort, render_template, flash
+                  abort, render_template, flash, json
 
+
+DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+last_card = ''
+
+def save_image(img):
+    f = open("current_hand.jpg", "wb")
+    f.write(img)
+    f.close
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        card_image  = request.files['image']
-        return render_template('index.html', attachment = card_image)
+        #ipdb.set_trace()
+        card_image = request.get_data()
+        image_file = save_image(card_image)
+        global last_card
+        last_card = card_image
+        return card_image
     else:
-        return render_template('index.html')
+
+        return last_card
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
